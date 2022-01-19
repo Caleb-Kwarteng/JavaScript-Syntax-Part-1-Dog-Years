663c85270265d48521dc7dbc8e279e778c10d48a
Caleb Buabeng Kwarteng <caleb.kwarteng@amalitech.org>
2022-01-19 16:43:38 +0000

    Create app.js
    Second Codecademy project - Dog years

---
+++ app.js
@@ -0,0 +1,28 @@
+//Dog Years
+//Declaation of age variable
+let myAge = 25;
+
+//Second variable Declaration 
+let earlyYears = 2;
+earlyYears = 2 * 5;
+
+//Years reduction
+let laterYears = myAge - 2;
+
+//Multiply later years
+laterYears *= 4;
+
+console.log(earlyYears);
+console.log(laterYears);
+
+//Calculating dog years
+let myAgeInDogYears = earlyYears + laterYears;
+
+//convert name to lower case
+let myName = "Caleb";
+myName.toLowerCase();
+
+//Final display of ages
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`)
+
+
